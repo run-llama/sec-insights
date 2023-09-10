3714c32c89ba9b551058b8848fe9799eab6a1614
siddarthreddygsr <siddarthreddygsr@gmail.com>
2023-09-11 02:03:41 +0530

    made requested changes

--- backend/wait-for-db.py
+++ backend/wait-for-db.py
@@ -1,15 +1,10 @@
-import os
 import asyncio
 import asyncpg
-import time
+from app.core.config import settings
 
-database_url = os.environ.get("DATABASE_URL")
 database_url = "postgresql://user:password@db:5432/llama_app_db"
-max_attempts = 30
-sleep_interval = 1
 
-async def check_database_connection():
-    print(database_url)
+async def check_database_connection(max_attempts: int = 30, sleep_interval: int = 1) -> bool:
     for attempt in range(1, max_attempts + 1):
         print(database_url + "hi")
         try:
@@ -20,7 +15,7 @@ async def check_database_connection():
             print(f"Attempt {attempt}: Database is not yet available. Error: {e}")
             if attempt == max_attempts:
                 print("Max attempts reached. Exiting.")
-                return True
+                return False
             await asyncio.sleep(sleep_interval)
 
 async def main():
