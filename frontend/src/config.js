e032175c3183daf293e6a85cb45b3921a32f9605
Sourabh Desai <sourabhdesai@gmail.com>
2023-09-28 09:15:55 +0000

    address variety of more issues with running on codespaces + issues with make migate

--- frontend/src/config.js
+++ frontend/src/config.js
@@ -1,3 +1,11 @@
 import { env } from "~/env.mjs";
+
+if (env.NEXT_PUBLIC_CODESPACES === 'true' && env.NEXT_PUBLIC_CODESPACE_NAME) {
+    const suggestedUrl = `https://${env.NEXT_PUBLIC_CODESPACE_NAME}-8000.app.github.dev/`;
+    if (!env.NEXT_PUBLIC_BACKEND_URL.startsWith(suggestedUrl)) {
+        console.warn(`It looks like you're running on a Github codespace. You may want to set the NEXT_PUBLIC_BACKEND_URL environment variable to ${suggestedUrl}`);
+    }
+}
+
 export const backendUrl = env.NEXT_PUBLIC_BACKEND_URL;
 
