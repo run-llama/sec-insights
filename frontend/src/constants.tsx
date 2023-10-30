a4c6ec88a4961edc372cad5f494600ae3ee252f7
Sourabh Desai <sourabhdesai@gmail.com>
2023-10-30 18:30:24 +0000

    fix ts error

--- frontend/src/constants.tsx
+++ frontend/src/constants.tsx
@@ -2,4 +2,4 @@ export const GOOGLE_ANALYTICS_ID = "G-LGHB46ZGWR";
 export const INTERCOM_ID = "rx71g1uo";
 // TODO: Populate with your own Sentry DSN:
 // https://docs.sentry.io/product/sentry-basics/concepts/dsn-explainer/
-export const SENTRY_DSN: string | null = null;
+export const SENTRY_DSN: string | undefined = undefined;
